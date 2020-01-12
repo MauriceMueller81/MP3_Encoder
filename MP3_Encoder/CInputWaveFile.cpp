7b52f3f0bf2d6b1de770a522c0e8d83e73c1435d
MauriceMueller81 <maurice_mueller667@web.de>
2020-01-12 15:41:36 +0100

    add class for Wave audio file

---
+++ MP3_Encoder/CInputWaveFile.cpp
@@ -0,0 +1,21 @@
+/*
+ * CInputWaveFile.cpp
+ *
+ *  Created on: Jan 12, 2020
+ *      Author: maurice
+ */
+
+#include "CInputWaveFile.h"
+
+namespace std {
+
+CInputWaveFile::CInputWaveFile() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CInputWaveFile::~CInputWaveFile() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace std */
---
+++ MP3_Encoder/CInputWaveFile.h
@@ -0,0 +1,21 @@
+/*
+ * CInputWaveFile.h
+ *
+ *  Created on: Jan 12, 2020
+ *      Author: maurice
+ */
+
+#ifndef CINPUTWAVEFILE_H_
+#define CINPUTWAVEFILE_H_
+
+namespace std {
+
+class CInputWaveFile {
+public:
+	CInputWaveFile();
+	virtual ~CInputWaveFile();
+};
+
+} /* namespace std */
+
+#endif /* CINPUTWAVEFILE_H_ */
